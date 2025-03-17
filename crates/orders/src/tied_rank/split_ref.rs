cca05b283775ebfc3e64e86c03ecb602c6f26741
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-03-17 11:48:37 +0100

    Add tests

--- crates/orders/src/tied_rank/split_ref.rs
+++ crates/orders/src/tied_rank/split_ref.rs
@@ -1,9 +1,9 @@
 use std::marker::PhantomData;
 
-// Stores two slices, a: `&[usize]` and b: `&[bool]`, but only one len.
-// We assume `a.len() == self.a_len` and `b.len() == self.a_len - 1`.
-//
-// Might be overoptimizing, ¯\_(ツ)_/¯
+/// Stores two slices, a: `&[usize]` and b: `&[bool]`, but only one len.
+/// We assume `a.len() == self.a_len` and `b.len() == self.a_len - 1`.
+///
+/// It does not guarantee anything about the slices' contents.
 #[derive(Debug, Clone, Copy)]
 pub(super) struct SplitRef<'a> {
     a_len: usize,
@@ -37,3 +37,50 @@ impl PartialEq for SplitRef<'_> {
 }
 
 impl Eq for SplitRef<'_> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let a = Vec::new();
+        let b = Vec::new();
+        let s = SplitRef::new(&a, &b);
+        assert!(s.a().is_empty());
+        assert!(s.b().is_empty());
+    }
+
+    #[test]
+    fn single() {
+        let a = vec![0];
+        let b = Vec::new();
+        let s = SplitRef::new(&a, &b);
+        assert!(s.a().len() == 1);
+        assert!(s.b().is_empty());
+    }
+
+    #[test]
+    fn two() {
+        let a = vec![1, 4241];
+        let b = vec![false];
+        let s = SplitRef::new(&a, &b);
+        assert!(s.a().len() == 2);
+        assert!(s.b().len() == 1);
+    }
+
+    #[test]
+    fn long() {
+        let a: [usize; 7] = [1, 4241, 4, 564, 233, 7, 2];
+        let b: [bool; 6] = [false, true, false, true, true, false];
+        let s = SplitRef::new(&a, &b);
+        for aa in a.iter().zip(s.a().iter()) {
+            assert!(aa.0 == aa.1);
+        }
+        for bb in b.iter().zip(s.b().iter()) {
+            assert!(bb.0 == bb.1);
+        }
+        assert!(s.a().len() == a.len());
+        assert!(s.b().len() == b.len());
+    }
+}
